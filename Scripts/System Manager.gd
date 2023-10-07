0e08278bacada5d172d3417b74426d916cf6984b
BladeBlaine7 <145941874+BladeBlaine7@users.noreply.github.com>
2023-10-07 16:52:58 -0700

    wa

--- Scripts/System Manager.gd
+++ Scripts/System Manager.gd
@@ -2,21 +2,10 @@ extends Node
 
 var mats = 0
 
-var PlayerHp = 1000;
-#CALL THIS BY USING Global.PlayerHp
-var PlayerHunger = 1000;
-#CALL THIS BY USING Global.PlayerHunger
 
-func set_HealthBar() -> void:
-	$HealthBar.value = PlayerHp;
-	
-func set_HungerBar() -> void:
-	$HungerBar.value = PlayerHunger;
-	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_HealthBar()
-	set_HungerBar()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
