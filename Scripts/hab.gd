4348fd684b9732e85c3c21d3744a11851a8c5fda
panopticon0 <aiden.314082@nv.ccsd.net>
2023-10-08 14:22:09 -0700

    persistent data for real this time

--- Scripts/hab.gd
+++ Scripts/hab.gd
@@ -4,7 +4,25 @@ var outHab = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Player.health_changed.connect($"/root/SystemManager".update_health)
+	$Player.oxygen_changed.connect($"/root/SystemManager".update_oxygen)
+	$Player.hunger_changed.connect($"/root/SystemManager".update_hunger)
+	$Player.malfunction.connect($"/root/SystemManager".update_malfunction)
+	$Player.malfunction.connect($UI.update_mal)
+	
+	$"Level Design".rock.connect($"/root/SystemManager".update_rock)
+	$"Level Design".water.connect($"/root/SystemManager".update_water)
+	$"Level Design".ice.connect($"/root/SystemManager".update_ice)
+	
+	$"Level Design".rock.connect($UI.update_rock)
+	$"Level Design".water.connect($UI.update_water)
+	$"Level Design".ice.connect($UI.update_ice)
+	
+	$"/root/SystemManager".healthSig.connect($UI.update_health)
+	$"/root/SystemManager".oxygenSig.connect($UI.update_oxygen)
+	$"/root/SystemManager".hungerSig.connect($UI.update_hunger)
+	
+	$Player.selectMatChange.connect($UI.update_select)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
