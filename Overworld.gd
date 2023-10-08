d56173e8feb65f458c0d7a64472937019c5912b6
BladeBlaine7 <145941874+BladeBlaine7@users.noreply.github.com>
2023-10-07 22:45:07 -0700

    hab area + map extension

--- Overworld.gd
+++ Overworld.gd
@@ -4,3 +4,13 @@ func _ready():
 	$Player.health_changed.connect($UI.update_health)
 	$Player.oxygen_changed.connect($UI.update_oxygen)
 	$Player.hunger_changed.connect($UI.update_hunger)
+
+
+func _on_area_2d_body_entered(body):
+	if (body.get_name() == "Player"):
+
+
+func _on_hab_area_body_entered(body):
+	if (body.get_name() == "Player"):
+		#MAKE A POP UP TO TELL PLAYER TO PRESS 'E'
+
